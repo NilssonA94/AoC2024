6ae8b9cffe7bd886fe096fdbf0c0f39b6a126ca2
NilssonA94 <anton.hiltunen94@gmail.com>
2024-12-14 12:35:19 +0100

    LIFE

--- Day9.cs
+++ Day9.cs
@@ -3,47 +3,91 @@
 public class Day9 : Day
 {
     public override long Expected1 { get; set; } = 1928;
-    public override long Expected2 { get; set; } = 0;
+    public override long Expected2 { get; set; } = 2858;
     public override void Run()
     {
-        string diskMap = "";
-        int index = 0;
-        for (int i = 0; i < Input[0].Length; i++)
+        List<(long index, long length, bool hasMoved)> map = [];
+        List<string> blocks = [];
+        long index = -1;
+
+        foreach (var block in Input[0])
         {
-            bool isFile = i % 2 == 0;
-            for (int j = 0; j < int.Parse(Input[0][i].ToString()); j++)
+            if (map.Count % 2 == 0 && block != '0') index++;
+            map.Add(map.Count % 2 == 0 ? (index, long.Parse(block.ToString()), false) : (-1, long.Parse(block.ToString()), true));
+        }
+
+        foreach (var block in map)
+        {
+            for (int i = 0; i < block.length; i++)
             {
-                if (isFile)
+                blocks.Add(block.index == -1 ? "." : block.index.ToString());
+            }
+        }
+
+        for (int i = blocks.Count - 1; i > 0; i--)
+        {
+            if (blocks[i] == ".") continue;
+            for (int j = 0; j < i; j++)
+            {
+                if (blocks[j] == ".")
                 {
-                    // file
-                    diskMap += index.ToString();
-                }
-                else
-                {
-                    // free space
-                    diskMap += ".";
+                    blocks[j] = blocks[i];
+                    blocks[i] = ".";
+                    break;
                 }
             }
-            if (isFile) index++;
         }
 
-        char[] diskArray = diskMap.ToCharArray();
-        for (int i = diskArray.Length - 1; i > 0; i--)
+        for (int i = map.Count - 1; i > 0; i--)
         {
-            if (diskArray[i] == '.') continue;
+            if (map[i].hasMoved) continue;
+            map[i] = (map[i].index, map[i].length, true);
 
-            int idx = Array.IndexOf(diskArray, '.');
+            for (int j = 0; j < map.Count - 1; j++)
+            {
+                if (j == i) break;
+                while (map[j].index == map[j + 1].index)
+                {
+                    map[j] = (-1, map[j].length + map[j + 1].length, true);
+                    map.RemoveAt(j + 1);
+                    i--;
+                }
+            }
 
-            if (idx > i) break;
-
-            diskArray[idx] = diskArray[i];
-            diskArray[i] = '.';
+            for (int j = 0; j < map.Count - 1; j++)
+            {
+                if (j == i) break;
+                if (map[j].index == -1 && map[j].length >= map[i].length)
+                {
+                    long difference = map[j].length - map[i].length;
+                    map[j] = map[i];
+                    map[i] = (-1, map[i].length, true);
+                    if (difference != 0)
+                    {
+                        map.Insert(j + 1, (-1, difference, true));
+                        i++;
+                    }
+                    break;
+                }
+            }
         }
-        diskMap = new string(diskArray).Replace(".", String.Empty);
 
-        for (int i = 0; i < diskMap.Length; i++)
+        for (int i = 0; i < blocks.Count; i++)
         {
-            Result1 += i * int.Parse(diskMap[i].ToString());
+            if (blocks[i] == ".") continue;
+            Result1 += i * int.Parse(blocks[i]);
+        }
+
+        long counter = -1;
+        for (int i = 0; i < map.Count; i++)
+        {
+            for (int j = 0; j < map[i].length; j++)
+            {
+                counter++;
+                if (map[i].index == -1) continue;
+
+                Result2 += counter * map[i].index;
+            }
         }
     }
 }
