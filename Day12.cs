56bbd73711f11979a90bed19421fe613ebfb36e7
NilssonA94 <anton.hiltunen94@gmail.com>
2024-12-19 18:34:23 +0100

    LIFE

--- Day12.cs
+++ Day12.cs
@@ -3,11 +3,13 @@
 public class Day12 : Day
 {
     public override long Expected1 { get; set; } = 1930;
-    public override long Expected2 { get; set; } = 0;
+    public override long Expected2 { get; set; } = 1206;
 
     private readonly HashSet<(int y, int x)> visited = [];
     public override void Run()
     {
+        Result1 = 0;
+        Result2 = 0;
         for (int i = 0; i < Input.Length; i++)
         {
             for (int j = 0; j < Input[i].Length; j++)
@@ -15,28 +17,39 @@ public class Day12 : Day
                 if (visited.Contains((i, j))) continue;
                 int area = 0;
                 int perimeter = 0;
-                Flood(i, j, ref area, ref perimeter);
-                Console.WriteLine($"Area type: {Input[i][j]}, Perimeter: {perimeter}, Area: {area}, Price: {area * perimeter}");
+                int sides = 0;
+                Flood(Input[i][j], i, j, ref area, ref perimeter, ref sides);
                 Result1 += area * perimeter;
+                Result2 += area * sides;
             }
         }
+        visited.Clear();
     }
 
-    public void Flood(int y, int x, ref int area, ref int perimeter)
+    public void Flood(char type, int y, int x, ref int area, ref int perimeter, ref int sides)
     {
+        if (visited.Contains((y, x))) return;
         visited.Add((y, x));
-        char type = Input[y][x];
-
         area++;
+        sides += CheckCorner(y, x);
+        if (y == 0 || Input[y - 1][x] != type) perimeter++; else Flood(type, y - 1, x, ref area, ref perimeter, ref sides);
+        if (x == Input[y].Length - 1 || Input[y][x + 1] != type) perimeter++; else Flood(type, y, x + 1, ref area, ref perimeter, ref sides);
+        if (y == Input.Length - 1 || Input[y + 1][x] != type) perimeter++; else Flood(type, y + 1, x, ref area, ref perimeter, ref sides);
+        if (x == 0 || Input[y][x - 1] != type) perimeter++; else Flood(type, y, x - 1, ref area, ref perimeter, ref sides);
+    }
 
-        if (y == 0 || Input[y - 1][x] != type) perimeter++; else if (!visited.Contains((y - 1, x))) Flood(y - 1, x, ref area, ref perimeter);
-        if (x == Input[y].Length - 1 || Input[y][x + 1] != type) perimeter++; else if (!visited.Contains((y, x + 1))) Flood(y, x + 1, ref area, ref perimeter);
-        if (y == Input.Length - 1 || Input[y + 1][x] != type) perimeter++; else if (!visited.Contains((y + 1, x))) Flood(y + 1, x, ref area, ref perimeter);
-        if (x == 0 || Input[y][x - 1] != type) perimeter++; else if (!visited.Contains((y, x - 1))) Flood(y, x - 1, ref area, ref perimeter);
+    public int CheckCorner(int i, int j)
+    {
+        int outsideCorners = ((i == 0 || Input[i][j] != Input[i - 1][j]) && (j == 0 || Input[i][j] != Input[i][j - 1]) ? 1 : 0) +
+                             ((i == 0 || Input[i][j] != Input[i - 1][j]) && (j == Input[i].Length - 1 || Input[i][j] != Input[i][j + 1]) ? 1 : 0) +
+                             ((i == Input.Length - 1 || Input[i][j] != Input[i + 1][j]) && (j == 0 || Input[i][j] != Input[i][j - 1]) ? 1 : 0) +
+                             ((i == Input.Length - 1 || Input[i][j] != Input[i + 1][j]) && (j == Input[i].Length - 1 || Input[i][j] != Input[i][j + 1]) ? 1 : 0);
 
-        /*if (y > 0 && Input[y - 1][x] == type && !visited.Contains((y - 1, x))) Flood(y - 1, x, ref area, ref perimeter);
-        if (x < Input[y].Length - 1 && Input[y][x + 1] == type && !visited.Contains((y, x + 1))) Flood(y, x + 1, ref area, ref perimeter);
-        if (y < Input.Length - 1 && Input[y + 1][x] == type && !visited.Contains((y + 1, x))) Flood(y + 1, x, ref area, ref perimeter);
-        if (x > 0 && Input[y][x - 1] == type && !visited.Contains((y, x - 1))) Flood(y, x - 1, ref area, ref perimeter);*/
+        int insideCorners = (i != 0 && j != Input[i].Length - 1 && Input[i][j] == Input[i - 1][j] && Input[i][j] == Input[i][j + 1] && Input[i][j] != Input[i - 1][j + 1] ? 1 : 0) +
+                             (i != Input.Length - 1 && j != Input[i].Length - 1 && Input[i][j] == Input[i + 1][j] && Input[i][j] == Input[i][j + 1] && Input[i][j] != Input[i + 1][j + 1] ? 1 : 0) +
+                             (i != Input.Length - 1 && j != 0 && Input[i][j] == Input[i + 1][j] && Input[i][j] == Input[i][j - 1] && Input[i][j] != Input[i + 1][j - 1] ? 1 : 0) +
+                             (i != 0 && j != 0 && Input[i][j] == Input[i - 1][j] && Input[i][j] == Input[i][j - 1] && Input[i][j] != Input[i - 1][j - 1] ? 1 : 0);
+
+        return outsideCorners + insideCorners;
     }
 }
